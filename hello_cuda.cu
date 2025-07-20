b3404cd2031aa7b00fc7159af26a9c58652cd0dc
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-20 10:02:13 +0530

    -

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -1,15 +1,22 @@
+each cuda core runs multiple threads at a time
 #include <iostream>
 
 // function qualifier marks the function as kernel i.e. it will be called from the cpu but run on gpu 
-// hello_cuda.exe -> executable binary file
-// hello_cuda.exp -> symbol export file
-// hello_cuda.lib -> static import library
+// here kernel only means a function that runs on gpu 
+
+/* hello_cuda.exe -> executable binary file
+ hello_cuda.exp -> symbol export file
+ hello_cuda.lib -> static import library */
 __global__ void helloFromGPU() {
     printf("Hello World from GPU!\n");
 }
 
+// when the program is compiled then the cpu and compiler are only working even when the program starts executing 
+/* but when there is a 'kernel launch' i.e. the kernel is called only then will the cpu give the gpu the 
+function to execute in parallel */
 int main() {
+    // <<how many blocks of threads to launch, threads per block>>(args);
     helloFromGPU<<<1, 1>>>();
-    cudaDeviceSynchronize();
+    cudaDeviceSynchronize(); // CUDA runtime api call which blocks the CPU until previously isshued GPU tasks aren't completed
     return 0;
 }
