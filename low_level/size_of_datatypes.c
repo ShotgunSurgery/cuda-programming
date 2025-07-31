39c5d38223bd5dd56d1474041170710fa0c4a004
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-31 22:55:47 +0530

    -

---
+++ low_level/size_of_datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("Size of int: %d", sizeof(int));
+    printf("\n Size of char: %d", sizeof(char));
+    printf("\n Size of float: %d", sizeof(float));
+    printf("\n Size of double: %d", sizeof(double));
+
+    // x++ use x then increment, --x decrement x then use 
+    // / -> divide, discard fraction; % output remainder
+}
